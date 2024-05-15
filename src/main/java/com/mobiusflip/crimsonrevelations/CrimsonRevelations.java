2ce2ecbf52184e431b16adc22ced0b397a34486d
Me <135455255+IcarussOne@users.noreply.github.com>
2024-05-15 16:15:16 -0500

    Formatting

--- src/main/java/com/mobiusflip/crimsonrevelations/CrimsonRevelations.java
+++ src/main/java/com/mobiusflip/crimsonrevelations/CrimsonRevelations.java
@@ -14,32 +14,28 @@ import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.research.ResearchCategories;
 
 @Mod(modid = CrimsonRevelations.MODID, name = CrimsonRevelations.NAME, version = CrimsonRevelations.VERSION, dependencies = CrimsonRevelations.DEPENDENCIES)
-public class CrimsonRevelations
-{
+public class CrimsonRevelations {
     public static final String MODID = "crimsonrevelations";
-    public static final String NAME = "Crimson Revelations";
-    public static final String VERSION = "0.8";
-    public static final String DEPENDENCIES = "required-after:thaumcraft";
+    public static final String NAME = "New Crimson Revelations";
+    public static final String VERSION = "1.0";
+    public static final String DEPENDENCIES = "required-after:thaumcraft@[1.12.2-6.1.BETA26,);after:thaumicaugmentation";
 
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         CrimsonRecipes.initRecipes();
-        ResearchCategories.registerCategory("CR", "CrimsonRites", new AspectList(), new ResourceLocation("thaumcraft", "textures/items/crimson_rites.png"), new ResourceLocation("thaumcraft", "textures/gui/gui_research_back_1.jpg"), new ResourceLocation("thaumcraft", "textures/gui/gui_research_back_over.png"));
+        ResearchCategories.registerCategory("CR", "CrimsonRites", new AspectList(), new ResourceLocation("thaumcraft", "textures/items/crimson_rites.png"), new ResourceLocation("crimsonrevelations", "textures/gui/research_background.jpg"), new ResourceLocation("thaumcraft", "textures/gui/gui_research_back_over.png"));
         ThaumcraftApi.registerResearchLocation(new ResourceLocation("crimsonrevelations", "research/revelations"));
     }
-    
+
     @EventHandler
-    public void postinit(FMLPostInitializationEvent event)
-    {
-    	
+    public void postinit(FMLPostInitializationEvent event) {
+
     }
 }
