6aa8ea4000680118aea32ea593ca4467fc60089b
Me <135455255+IcarussOne@users.noreply.github.com>
2024-05-16 17:09:53 -0500

    Loot table handler

---
+++ src/main/java/com/mobiusflip/crimsonrevelations/init/LootTableHandler.java
@@ -0,0 +1,9 @@
+package com.mobiusflip.crimsonrevelations.init;
+
+import com.mobiusflip.crimsonrevelations.CrimsonRevelations;
+
+import net.minecraft.util.ResourceLocation;
+
+public class LootTableHandler {
+    public static final ResourceLocation CULTIST = new ResourceLocation(CrimsonRevelations.MODID, ("entities/cultist"));
+}
