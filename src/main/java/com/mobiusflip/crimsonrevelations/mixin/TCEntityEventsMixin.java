e44eed23957f6af0e416bd931497be4baf765f86
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-05-22 16:41:15 +0200

    Fix entity events mixin

--- src/main/java/com/mobiusflip/crimsonrevelations/mixin/TCEntityEventsMixin.java
+++ src/main/java/com/mobiusflip/crimsonrevelations/mixin/TCEntityEventsMixin.java
@@ -1,21 +1,19 @@
 package com.mobiusflip.crimsonrevelations.mixin;
 
+import com.mobiusflip.crimsonrevelations.entity.boss.EntityOvergrownTaintacle;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.mobiusflip.crimsonrevelations.entity.boss.EntityOvergrownTaintacle;
-
-import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import thaumcraft.common.lib.events.EntityEvents;
 
 @Mixin(value = EntityEvents.class, remap = false)
 public class TCEntityEventsMixin {
     @Inject(method = "livingDrops", at = @At(value = "HEAD"), cancellable = true)
-    public void crLivingDrops(LivingDropsEvent event, CallbackInfo ci)
-    {
-    	// No hardcoded treasure bag drop
-        if (!event.getEntity().world.isRemote && event.isRecentlyHit() && event.getEntity() instanceof EntityOvergrownTaintacle) ci.cancel();
+    private static void crLivingDrops(LivingDropsEvent event, CallbackInfo ci) {
+        // No hardcoded treasure bag drop
+        if (!event.getEntity().world.isRemote && event.isRecentlyHit() && event.getEntity() instanceof EntityOvergrownTaintacle)
+            ci.cancel();
     }
 }
