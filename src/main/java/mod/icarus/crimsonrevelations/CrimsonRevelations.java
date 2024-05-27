f1ee81eea0cab6a2f747ed8ac254b4391f5a23f3
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-05-27 08:04:05 +0200

    Update project, use tags

--- src/main/java/mod/icarus/crimsonrevelations/CrimsonRevelations.java
+++ src/main/java/mod/icarus/crimsonrevelations/CrimsonRevelations.java
@@ -13,9 +13,9 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = CrimsonRevelations.MODID, name = CrimsonRevelations.NAME, version = CrimsonRevelations.VERSION, dependencies = CrimsonRevelations.DEPENDENCIES)
 public class CrimsonRevelations {
-    public static final String MODID = "crimsonrevelations";
+    public static final String MODID = Tags.MOD_ID;
     public static final String NAME = "New Crimson Revelations";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = Tags.VERSION;
     public static final String DEPENDENCIES = "required-after:mixinbooter@[8.0,);required-after:thaumcraft@[1.12.2-6.1.BETA26,);after:thaumicaugmentation";
     public static final CreativeTabs tabCR = new CRCreativeTabs(CreativeTabs.CREATIVE_TAB_ARRAY.length, "CrimsonRevelationsTab");
 
