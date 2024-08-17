f83d56b3372405986addf49bf06fa991aef9d39a
Me <135455255+IcarussOne@users.noreply.github.com>
2024-08-17 17:55:28 -0500

    Forgot to update the render

--- src/main/java/mod/icarus/crimsonrevelations/client/renderer/RenderPrimalArrow.java
+++ src/main/java/mod/icarus/crimsonrevelations/client/renderer/RenderPrimalArrow.java
@@ -10,12 +10,12 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class RenderPrimalArrow extends RenderArrow<EntityPrimalArrow> {
-    public static final ResourceLocation TEXTURE_AIR = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/primal_arrow_air.png");
-    public static final ResourceLocation TEXTURE_EARTH = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/primal_arrow_earth.png");
-    public static final ResourceLocation TEXTURE_ENTROPY = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/primal_arrow_entropy.png");
-    public static final ResourceLocation TEXTURE_FIRE = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/primal_arrow_fire.png");
-    public static final ResourceLocation TEXTURE_ORDER = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/primal_arrow_order.png");
-    public static final ResourceLocation TEXTURE_WATER = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/primal_arrow_water.png");
+    public static final ResourceLocation TEXTURE_AER = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/arrow_aer.png");
+    public static final ResourceLocation TEXTURE_AQUA = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/arrow_aqua.png");
+    public static final ResourceLocation TEXTURE_IGNIS = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/arrow_ignis.png");
+    public static final ResourceLocation TEXTURE_ORDO = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/arrow_ordo.png");
+    public static final ResourceLocation TEXTURE_PERDITIO = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/arrow_perditio.png");
+    public static final ResourceLocation TEXTURE_TERRA = new ResourceLocation(CrimsonRevelations.MODID, "textures/entity/arrow/arrow_terra.png");
 
     public RenderPrimalArrow(RenderManager renderManager) {
         super(renderManager);
@@ -25,18 +25,18 @@ public class RenderPrimalArrow extends RenderArrow<EntityPrimalArrow> {
     protected ResourceLocation getEntityTexture(EntityPrimalArrow entity) {
         switch (entity.getArrowType()) {
             case 5:
-                return TEXTURE_ENTROPY;
+                return TEXTURE_TERRA;
             case 4:
-                return TEXTURE_ORDER;
+                return TEXTURE_PERDITIO;
             case 3:
-                return TEXTURE_WATER;
+                return TEXTURE_ORDO;
             case 2:
-                return TEXTURE_FIRE;
+                return TEXTURE_IGNIS;
             case 1:
-                return TEXTURE_EARTH;
+                return TEXTURE_AQUA;
             case 0:
             default:
-                return TEXTURE_AIR;
+                return TEXTURE_AER;
         }
     }
 }
