d3486e483cb105bfadc72b3caff389ff5c2e28ab
Me <135455255+IcarussOne@users.noreply.github.com>
2025-02-03 23:43:59 -0600

    Add mana beans to pech loot tables [WIP] and bump version
    Pechs should preferably drop primal aspect beans only.

--- src/main/java/mod/icarus/crimsonrevelations/init/CRLootTables.java
+++ src/main/java/mod/icarus/crimsonrevelations/init/CRLootTables.java
@@ -9,6 +9,7 @@ import net.minecraft.world.storage.loot.conditions.LootCondition;
 import net.minecraft.world.storage.loot.functions.LootFunction;
 import net.minecraft.world.storage.loot.functions.LootingEnchantBonus;
 import net.minecraft.world.storage.loot.functions.SetCount;
+import net.minecraft.world.storage.loot.functions.SetNBT;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -16,13 +17,15 @@ import thaumcraft.Thaumcraft;
 
 @EventBusSubscriber(modid = NewCrimsonRevelations.MODID)
 public class CRLootTables {
+    // Loot Tables
     public static final ResourceLocation LESSER_CULTIST_PORTAL = new ResourceLocation(NewCrimsonRevelations.MODID, ("entities/lesser_cultist_portal"));
 
-    // Bosses
+    // Boss Loot Tables
     public static final ResourceLocation OVERGROWN_TAINTACLE = new ResourceLocation(NewCrimsonRevelations.MODID, ("entities/boss/overgrown_taintacle"));
 
     @SubscribeEvent
     public static void onLootTableLoad(LootTableLoadEvent event) {
+        // Crimson Cultists
         if (event.getName().equals(new ResourceLocation(Thaumcraft.MODID, "cultist"))) {
             LootPool crimson_material_pool = event.getTable().getPool("crimson_material");
 
@@ -43,6 +46,26 @@ public class CRLootTables {
             }
         }
 
+        // Pechs
+        if (event.getName().equals(new ResourceLocation(Thaumcraft.MODID, "pech"))) {
+            LootPool mana_bean_pool = event.getTable().getPool("mana_bean");
+
+            if (mana_bean_pool == null) {
+                mana_bean_pool = new LootPool(new LootEntry[0], new LootCondition[0], new RandomValueRange(1, 1), new RandomValueRange(1, 1), "mana_bean");
+                event.getTable().addPool(mana_bean_pool);
+            }
+
+            // TODO: Only make pechs drop primal aspect mana beans
+            if (mana_bean_pool != null) {
+                mana_bean_pool.addEntry(new LootEntryItem((new ItemStack(CRItems.MANA_BEAN).getItem()), 1, 0,
+                        new LootFunction[]{new SetCount(new LootCondition[]{new KilledByPlayer(false)}, new RandomValueRange(0, 1)),
+                                new SetNBT(new LootCondition[0], null),
+                                new LootingEnchantBonus(new LootCondition[0], new RandomValueRange(0, 1), 3)},
+                        new LootCondition[0], "crimsonrevelations:mana_bean"));
+            }
+        }
+
+        // Structures
         if (event.getName().equals(LootTableList.CHESTS_DESERT_PYRAMID) || event.getName().equals(LootTableList.CHESTS_SIMPLE_DUNGEON)) {
             LootPool main = event.getTable().getPool("main");
             main.addEntry(new LootEntryItem(CRItems.PROTECTION_RING, 5, 0, new LootFunction[0], new LootCondition[0], "loottable:protection_ring"));
