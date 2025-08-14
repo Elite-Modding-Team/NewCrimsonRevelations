4fa22bfd361745c0beeabbe074d930a1561d0bce
Me <135455255+IcarussOne@users.noreply.github.com>
2025-08-14 03:19:46 -0500

    Better mixin check

--- src/main/java/mod/icarus/crimsonrevelations/mixin/entity/vanilla/EntityLivingBaseMixin.java
+++ src/main/java/mod/icarus/crimsonrevelations/mixin/entity/vanilla/EntityLivingBaseMixin.java
@@ -5,15 +5,17 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import thaumcraft.common.items.tools.ItemThaumometer;
 
 @Mixin(EntityLivingBase.class)
 public abstract class EntityLivingBaseMixin extends Entity {
@@ -24,7 +26,7 @@ public abstract class EntityLivingBaseMixin extends Entity {
     // Prevents scanning from occasionally playing equip sounds.
     @Inject(method = "playEquipSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;playSound(Lnet/minecraft/util/SoundEvent;FF)V", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
     protected void playEquipSoundInject(ItemStack stack, CallbackInfo ci, SoundEvent soundEvent, Item item) {
-        if (stack.getItem() instanceof ItemThaumometer && !Loader.isModLoaded("oldresearch") && CRConfig.general_settings.THAUMOMETER_OLD_SCANNING) {
+        if (item == ForgeRegistries.ITEMS.getValue(new ResourceLocation("thaumcraft:thaumometer")) && !Loader.isModLoaded("oldresearch") && CRConfig.general_settings.THAUMOMETER_OLD_SCANNING) {
             ci.cancel();
         }
     }
