9211587399f8be1fdd31ea7dae16637d1919793f
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-02-13 02:01:02 -0600

    Hopefully fix a crash with Vis Attunement

--- src/main/java/mod/icarus/crimsonrevelations/mixin/misc/CasterManagerMixin.java
+++ src/main/java/mod/icarus/crimsonrevelations/mixin/misc/CasterManagerMixin.java
@@ -14,14 +14,17 @@ import thaumcraft.common.lib.enchantment.EnumInfusionEnchantment;
 public class CasterManagerMixin {
     @ModifyReturnValue(method = "getTotalVisDiscount", at = @At("RETURN"))
     private static float visAttunementInfusionAdjustmentMixin(float original, @Local(ordinal = 0, argsOnly = true) EntityPlayer player) {
+        if(player == null) return original;
+
+        int extraDiscount = 0;
         for(ItemStack armor : player.inventory.armorInventory) {
             if(!armor.isEmpty()) {
                 int level = EnumInfusionEnchantment.getInfusionEnchantmentLevel(armor, CREnchantments.VIS_ATTUNEMENT);
                 if(level > 0) {
-                    original += level;
+                    extraDiscount += level;
                 }
             }
         }
-        return original;
+        return original + (extraDiscount / 100f);
     }
 }
